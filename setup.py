48bdb40f2ab832e0eb4e196266cf794b848333bd
HAKEEM AMUSAT <aoamusat@gmail.com>
2020-04-07 17:12:40 +0100

    Configured setup tool

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
 from setuptools import setup, find_packages
 
 
-setup(name="covid19-estimator-py", packages=find_packages())
+setup(
+    name="covid19-estimator-py",
+    author = "Akeem Amusat",
+    author_email = "aoamusat@gmail.com",
+    description = ("#Build4SDG Challenge"),
+    packages=find_packages()
+)
